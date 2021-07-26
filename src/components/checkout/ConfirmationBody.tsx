bdcdca4f0da162248f56a3407f7f9ea29c446e45
Bruno Cunha <bruncun@icloud.com>
2021-07-26 13:42:52 -0400

    Implement confirmation purchase summary collapse

--- src/components/checkout/ConfirmationBody.tsx
+++ src/components/checkout/ConfirmationBody.tsx
@@ -1,20 +1,13 @@
-import ConfirmationProduct from "components/checkout/ConfirmationProduct";
-import CartContext from "CartContext";
-import { useContext } from "react";
-import { useProductsById } from "hooks/useApi";
-import { formatter } from "utils";
+import MobilePurchaseSummary from "components/checkout/MobilePurchaseSummary";
+import TabletPurchaseSummary from "components/checkout/TabletPurchaseSummary";
+import { useState } from "react";
 
 function ConfirmationBody() {
-  const { getCosts, cart, selectedProductIds } = useContext(CartContext);
-  const selectedProductQueries = useProductsById(selectedProductIds);
+  const [showAllProducts, setShowAllProducts] = useState(false);
 
-  if (selectedProductQueries.some(({ isLoading }) => isLoading)) return <></>;
-
-  const selectedProducts = selectedProductQueries.map(
-    ({ data }) => data as Product
-  );
-  const firstSelectedProduct = selectedProducts[0];
-  const { total } = getCosts(selectedProducts);
+  function onToggleShowAllProductsClick() {
+    setShowAllProducts(!showAllProducts);
+  }
 
   return (
     <>
@@ -33,38 +26,16 @@ function ConfirmationBody() {
         You will receive an email confirmation shortly.
       </p>
       <div className="d-md-none">
-        <div className="rounded-top bg-light d-flex p-4 flex-column">
-          <ul className="list-unstyled mb-2">
-            <ConfirmationProduct
-              product={firstSelectedProduct}
-              quantity={cart[firstSelectedProduct.id]}
-            />
-          </ul>
-          <span className="text-black-50 fw-bold text-center mb-0 fs-7 mt-1">
-            and {selectedProductIds.length - 1} other item(s)
-          </span>
-        </div>
-        <div className="rounded-bottom bg-dark d-flex px-4 py-3 flex-column">
-          <span className="text-white-50 text-uppercase mb-2">Grand Total</span>
-          <h6 className="text-white fw-bold mb-0">{formatter.format(total)}</h6>
-        </div>
+        <MobilePurchaseSummary
+          onToggleShowAllProductsClick={onToggleShowAllProductsClick}
+          showAllProducts={showAllProducts}
+        />
       </div>
       <div className="d-none d-md-flex w-100">
-        <div className="rounded-start bg-light d-flex p-4 flex-column w-100">
-          <ul className="list-unstyled">
-            <ConfirmationProduct
-              product={firstSelectedProduct}
-              quantity={cart[firstSelectedProduct.id]}
-            />
-          </ul>
-          <span className="text-black-50 fw-bold text-center mb-0 fs-7 mt-1">
-            and {selectedProductIds.length - 1} other item(s)
-          </span>
-        </div>
-        <div className="rounded-end bg-dark d-flex p-4 flex-column w-75 justify-content-center">
-          <span className="text-white-50 text-uppercase mb-2">Grand Total</span>
-          <h6 className="text-white fw-bold mb-0">{formatter.format(total)}</h6>
-        </div>
+        <TabletPurchaseSummary
+          onToggleShowAllProductsClick={onToggleShowAllProductsClick}
+          showAllProducts={showAllProducts}
+        />
       </div>
     </>
   );
---
+++ src/components/checkout/MobilePurchaseSummary.tsx
@@ -0,0 +1,75 @@
+import ConfirmationProduct from "components/checkout/ConfirmationProduct";
+import CartContext from "CartContext";
+import { useContext } from "react";
+import { useProductsById } from "hooks/useApi";
+import { formatter } from "utils";
+
+interface MobilePurchaseSummaryProps {
+  showAllProducts: boolean;
+  onToggleShowAllProductsClick: () => void;
+}
+
+function MobilePurchaseSummary({
+  showAllProducts,
+  onToggleShowAllProductsClick,
+}: MobilePurchaseSummaryProps) {
+  const { getCosts, cart, selectedProductIds } = useContext(CartContext);
+  const selectedProductQueries = useProductsById(selectedProductIds);
+
+  if (selectedProductQueries.some(({ isLoading }) => isLoading)) return <></>;
+
+  const selectedProducts = selectedProductQueries.map(
+    ({ data }) => data as Product
+  );
+  const firstSelectedProduct = selectedProducts[0];
+  const { total } = getCosts(selectedProducts);
+
+  return (
+    <>
+      <div className="rounded-top bg-light d-flex p-4 flex-column">
+        <ul className="list-unstyled mb-2">
+          {showAllProducts ? (
+            selectedProducts.map((product, index) => (
+              <ConfirmationProduct
+                key={index}
+                product={product}
+                quantity={cart[product.id]}
+              />
+            ))
+          ) : (
+            <ConfirmationProduct
+              product={firstSelectedProduct}
+              quantity={cart[firstSelectedProduct.id]}
+            />
+          )}
+        </ul>
+        <button
+          onClick={onToggleShowAllProductsClick}
+          className="btn btn-link p-0 text-decoration-none text-capitalize ls-0 border-top pt-2"
+        >
+          <span className="text-black-50 fw-bold text-center mb-0 fs-7 mt-1">
+            {showAllProducts ? (
+              <>
+                View <span className="text-lowercase">less</span>
+              </>
+            ) : (
+              <>
+                And{" "}
+                <span className="text-lowercase">
+                  {selectedProductIds.length - 1} other item
+                  {selectedProductIds.length === 2 ? "" : "s"}
+                </span>
+              </>
+            )}
+          </span>
+        </button>
+      </div>
+      <div className="rounded-bottom bg-dark d-flex px-4 py-3 flex-column">
+        <span className="text-white-50 text-uppercase mb-2">Grand Total</span>
+        <h6 className="text-white fw-bold mb-0">{formatter.format(total)}</h6>
+      </div>
+    </>
+  );
+}
+
+export default MobilePurchaseSummary;
