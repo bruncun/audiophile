3acc6e866f24534d8b494bd56578d77b01ef28cc
Bruno Cunha <bruncun@icloud.com>
2021-07-30 19:16:46 -0400

    remove outline from offcanvas nav

--- src/components/navbar/OffcanvasNav.tsx
+++ src/components/navbar/OffcanvasNav.tsx
@@ -16,7 +16,7 @@ function OffcanvasNav() {
       tabIndex={-1}
       aria-label="navbar"
       aria-describedby="OffcanvasHeader"
-      className="fixed-top bg-white rounded-bottom d-xxl-none pt-2 py-md-5 mt-6 overflow-scroll z-9999"
+      className="fixed-top bg-white rounded-bottom d-xxl-none pt-2 py-md-5 mt-6 overflow-scroll z-9999 offcanvas-nav"
     >
       <div className="container-md pt-5 pt-md-2">
         <div className="pt-1 pb-6 mb-4 mb-md-2 pb-md-2 pt-md-4 mt-md-1">
