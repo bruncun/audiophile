f5355f8c2af0c48d86b6df3926b57f4475bc2593
Bruno Cunha <bruncun@icloud.com>
2021-08-03 08:39:00 -0400

    remove fixed positioning from nav

--- src/components/navbar/OffcanvasNav.tsx
+++ src/components/navbar/OffcanvasNav.tsx
@@ -1,23 +1,8 @@
 import CategoryList from "components/shared/CategoryList";
-import { useEffect, useRef } from "react";
 
 function OffcanvasNav() {
-  const offcanvasNavEl = useRef<HTMLDivElement>(null);
-
-  useEffect(function () {
-    if (!offcanvasNavEl.current) throw Error("offcanvasNavEl is not assigned");
-    offcanvasNavEl.current.focus();
-  });
-
   return (
-    <div
-      ref={offcanvasNavEl}
-      role="dialog"
-      tabIndex={-1}
-      aria-label="navbar"
-      aria-describedby="OffcanvasHeader"
-      className="fixed-top bg-white rounded-bottom d-xxl-none pt-2 py-md-5 mt-6 overflow-scroll z-9999 offcanvas-nav"
-    >
+    <div className="bg-white rounded-bottom d-xxl-none pt-2 py-md-5 overflow-auto">
       <div className="container-md pt-5 pt-md-2">
         <div className="pt-1 pb-6 mb-4 mb-md-2 pb-md-2 pt-md-4 mt-md-1">
           <CategoryList />
