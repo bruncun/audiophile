56ef57901e00350e9aaead073a250cab31e8fddf
Bruno Cunha <bruncun@icloud.com>
2021-08-04 16:23:44 -0400

    add more aspect ratio boxes

--- src/components/product/Gallery.tsx
+++ src/components/product/Gallery.tsx
@@ -15,27 +15,37 @@ function Gallery({
       <div className="col-md-5 d-md-flex">
         <div className="d-flex align-items-start flex-column w-100">
           <div className="w-100 mb-xxl-auto">
-            <ResponsiveImage
-              image={first}
-              imgClassName="rounded img-fluid w-100 mb-3 pb-1 mb-xxl-auto"
-              alt={name}
-            />
+            <div className="mb-3 pb-1 mb-xxl-auto">
+              <div className="small-gallery-image-wrapper position-relative overflow-hidden">
+                <ResponsiveImage
+                  image={first}
+                  imgClassName="rounded position-absolute top-0 start-0 w-100"
+                  alt={name}
+                />
+              </div>
+            </div>
           </div>
           <div className="w-100">
-            <ResponsiveImage
-              image={second}
-              imgClassName="rounded img-fluid mb-3 pb-1 pb-md-0 mb-md-0 w-100"
-              alt={name}
-            />
+            <div className="mb-3 pb-1 pb-md-0 mb-md-0">
+              <div className="small-gallery-image-wrapper position-relative overflow-hidden">
+                <ResponsiveImage
+                  image={second}
+                  imgClassName="rounded position-absolute top-0 start-0 w-100"
+                  alt={name}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div className="col-md-7">
-        <ResponsiveImage
-          image={third}
-          imgClassName="rounded img-fluid w-100"
-          alt={name}
-        />
+        <div className="large-gallery-image-wrapper position-relative overflow-hidden">
+          <ResponsiveImage
+            image={third}
+            imgClassName="rounded position-absolute top-0 start-0 w-100"
+            alt={name}
+          />
+        </div>
       </div>
     </div>
   );
